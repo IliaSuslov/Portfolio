9388c5f3c53c83515d391816343b6e7156dc9871
IliaSuslov <isuslov@me.com>
2020-08-25 15:45:54 +0300

    input way
    input way

--- src/small_projects/camera/camera.js
+++ src/small_projects/camera/camera.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import Camera from 'react-html5-camera-photo';
-import 'react-html5-camera-photo/build/css/index.css';
 
 function CameraWeb(props) {
-    function handleTakePhoto(dataUri) {
-        // Do stuff with the photo...
-        alert("takePhoto");
-    }
-
     return (
-        <Camera
-            onTakePhoto={(dataUri) => { handleTakePhoto(dataUri); }}
-        />
+        <input type="file" accept="image/*" capture="environment"/>
     );
 }
 export default CameraWeb
