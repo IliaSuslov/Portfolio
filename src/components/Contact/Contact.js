d15e2d1230aca5d416a07f992ca963f642206c2d
IliaSuslov <isuslov@me.com>
2020-08-26 18:26:21 +0300

    email btn
    email btn

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { Button } from 'reactstrap';
 import "./contact.css"
 import "../../App.css"
 
 const Contact = () => {
+
+    const handleClick = () => {
+        let email = "isuslov@me.com";
+        window.location.href = `mailto:${email}`;
+    }
+
     return (
         <div>
             <h1 id="contact" className="heading">Contact Me</h1>
             <div className="item">
-                <p>Email: isuslov@me.com</p>
+                <Button color="success" onClick={handleClick}>Email Me</Button>
                 <p>Skype: live:isuslov_1</p>
             </div>
         </div>
