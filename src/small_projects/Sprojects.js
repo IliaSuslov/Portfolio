d4f06a18bbd0e87e5cc88d8cebcaf3e4d208d2e3
IliaSuslov <isuslov@me.com>
2020-08-26 18:44:24 +0300

    sm back
    sm

--- src/small_projects/Sprojects.js
+++ src/small_projects/Sprojects.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './sprojects.css';
 import Navbar from './header/header';
-// import FrameWorks from './card/data';
-// import ApiTest from './api_test/api_test';
-// import Calculator from './calculator/calculator';
+import FrameWorks from './card/data';
+import ApiTest from './api_test/api_test';
+import Calculator from './calculator/calculator';
 import Camera from './camera/camera';
 
 function App() {
   return (
     <div className="sprojects">
       <Navbar />
-      {/* <FrameWorks /> */}
-      {/* <ApiTest /> */}
-      {/* <Calculator /> */}
-      <Camera/>
+      <FrameWorks />
+      <ApiTest />
+      <Calculator />
+      <Camera />
     </div>
   );
 }
