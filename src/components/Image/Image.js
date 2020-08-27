60b52e3116bd88513b4e4ac3f08e8d76a6ac4410
IliaSuslov <isuslov@me.com>
2020-08-27 10:05:52 +0300

    im
    im

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -9,7 +9,7 @@ const Image = () => {
                 <div className="name">
                     <h1>Ilia Suslov</h1>
                 </div>
-                <h2>Web developer</h2>
+                <h2>Front-End developer</h2>
                 <Links />
             </div>
         </div>
