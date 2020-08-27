02cdf289c7182128a69f5269bfa8d199738bf4c4
IliaSuslov <isuslov@me.com>
2020-08-27 11:32:18 +0300

    smooth scroll
    smooth scroll

--- src/components/Navbar/DesktopNB.js
+++ src/components/Navbar/DesktopNB.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 import "./nav.css";
 
 const DesktopNB = () => {
     return (
         <header className="Header">
             <nav className="Nav">
-                <a href="#home">Home</a>
-                <a href="#about">About Me</a>
-                <a href="#skills">Skills</a>
-                <a href="#works">Works</a>
+                <AnchorLink href="#home">Home</AnchorLink>
+                <AnchorLink href="#about">About Me</AnchorLink>
+                <AnchorLink href="#skills">Skills</AnchorLink>
+                <AnchorLink href="#works">Works</AnchorLink>
             </nav>
         </header>
     )
 }
 export default DesktopNB;
+
